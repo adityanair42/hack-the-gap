e875c4d9856c2762d8b778b15dec5052428998fb
adityanair42 <adityamnair42@gmail.com>
2025-09-08 11:48:12 +0000

    fixed issue1

--- http-backend/src/index.ts
+++ http-backend/src/index.ts
@@ -1,10 +1,19 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.post("signup", async (req, res) => {
-  
-})
+app.post("/signup", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  res.json("Signup req received");
+});
+
+app.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  res.json("Login req received");
+});
+
+app.listen(1234);
